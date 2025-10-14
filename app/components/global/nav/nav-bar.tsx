ef5efbf585e30b7a98df0943de39c5494563d477
M-Itrych <michal.itrych1312@gmail.com>
2025-10-14 20:35:29 +0200

    fix

--- app/components/global/nav/nav-bar.tsx
+++ app/components/global/nav/nav-bar.tsx
@@ -1,13 +1,21 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import React from "react";
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = React.useState(true);
     return (
-        <div className="w-[300px] h-screen bg-[#EEF1EE]">
-            <div className="w-full flex flex-row bg-[#608858] gap-8 items-center p-4">
-                <Image src="/logo_szkoly.png" alt="logo" width={40} height={40} className="w-[40px] h-[40px] bg-white rounded-full" />
-                <h1 className="text-white text-l font-bold text-center">Przedszkole nr14<br />"Biały Żagiel"</h1>
-
+        <div className={`${isOpen ? "w-[300px]" : "w-[80px]"} h-screen bg-[#EEF1EE] transition-all duration-300`}>
+            <div className="w-full flex flex-row bg-[#608858] h-[100px] gap-8 items-center py-4 px-2 justify-center">
+                {isOpen ? <Image src="/logo_szkoly.png" alt="logo" width={40} height={40} className="w-[40px] h-[40px] bg-white rounded-full" /> : null}
+                {isOpen ? <h1 className="text-white text-l font-bold text-center">Przedszkole nr14<br />"Biały Żagiel"</h1> : null}
+                {isOpen 
+                ? <ArrowBackIosIcon className="text-white text-3xl cursor-pointer" onClick={() => {setIsOpen(!isOpen)}}/> 
+                : <ArrowForwardIosIcon className="text-white text-3xl cursor-pointer" onClick={() => {setIsOpen(!isOpen)}}/>
+                }
             </div>
         </div>
     );
