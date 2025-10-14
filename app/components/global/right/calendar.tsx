8d76a11a74bc95dfb8f0bb0eb70551080ffb48e9
Wizrat-KJ <wizarddq@gmail.com>
2025-10-14 21:06:38 +0200

    kalendarz fix ficzur

--- app/components/global/right/calendar.tsx
+++ app/components/global/right/calendar.tsx
@@ -2,33 +2,23 @@
 
 import * as React from "react"
 import { Calendar } from "@/components/ui/calendar"
-import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { pl } from "react-day-picker/locale"
 
-export function Calendar13() {
-  const [dropdown, setDropdown] =
-    React.useState<React.ComponentProps<typeof Calendar>["captionLayout"]>("dropdown")
-  const [date, setDate] = React.useState<Date | undefined>(new Date(2025, 5, 12))
+export function Kalendarz() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
 
   return (
-    <div className="flex flex-col border-b-2 border-[#608858] pb-5 mb-5  w-fit h-fit">
+    <div className="flex flex-col border-b-2 border-[#608858] pb-5 mb-5 w-fit h-fit">
       <Calendar
         mode="single"
         defaultMonth={date}
         selected={date}
         onSelect={setDate}
-        captionLayout={dropdown}
-        className="rounded-lg border-[#608858] border shadow-sm p-3 "
+        locale={pl}
+        className="rounded-lg border-[#608858] border shadow-sm p-3"
         classNames={{
-    day_button: "data-[selected-single=true]:bg-blue-500 data-[selected-single=true]:text-white data-[selected-single=true]:hover:bg-blue-600"
-  }}
-        
+          day_button: "data-[selected-single=true]:bg-[#608858] data-[selected-single=true]:text-white data-[selected-single=true]:hover:bg-blue-600"
+        }}
       />
     </div>
   )
