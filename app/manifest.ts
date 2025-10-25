9cdda64968d114cecc580dd9fae38247474e0b18
M-Itrych <michal.itrych1312@gmail.com>
2025-10-25 17:17:25 +0200

    PWA changes

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,9 +2,9 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js PWA',
-    short_name: 'NextPWA',
-    description: 'A Progressive Web App built with Next.js',
+    name: 'Przedszkole nr 14 "Biały Żagiel"',
+    short_name: 'Przedszkole nr 14 "Biały Żagiel"',
+    description: 'Aplikacja dla rodziców i pracowników przedszkola',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
